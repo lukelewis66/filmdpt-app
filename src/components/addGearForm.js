42aedf001426a4ae59ff2f1043dc9128b84396b8
Luke Lewis <luke@Lukes-MacBook-Pro.local>
2020-08-29 01:35:35 -0700

    before changing AddGearForm to functional component

--- src/components/addGearForm.js
+++ src/components/addGearForm.js
@@ -3,21 +3,24 @@ import { Form, Select } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 
 class AddGearForm extends Component {
-  state = {
-    name: "",
-    level: "1",
-    available: "1",
+  //   state = {
+  //     name: "",
+  //     level: "1",
+  //     available: "1",
+  //   };
+
+  handleSubmit = () => {
+    //I want to call this from parent modal
   };
 
-  handleSubmit = () => {};
-
-  handleChange = (e, { name, value }) => {
-    this.setState({ [name]: value });
-    console.log("State changed: ", name, value);
-  };
+  //any changes to form inputs will set their corresponding state
+  //   handleChange = (e, { name, value }) => {
+  //     this.setState({ [name]: value });
+  //     console.log("State changed: ", name, value);
+  //   };
 
   render() {
-    const { name, level, available } = this.state;
+    const { name, level, available, handleChange } = this.props;
     const levelOptions = [
       { key: "1", value: "1", text: "Film 1" },
       { key: "2", value: "2", text: "Film 2" },
@@ -28,6 +31,8 @@ class AddGearForm extends Component {
       { key: "1", value: "1", text: "Yes" },
       { key: "0", value: "0", text: "No" },
     ];
+    console.log("handle change: ", handleChange);
+    console.log("level: ", level);
     return (
       <Form onSubmit={this.handleSubmit}>
         <Form.Field>
@@ -36,7 +41,8 @@ class AddGearForm extends Component {
             placeholder="Name"
             name="name"
             value={name}
-            onChange={this.handleChange}
+            onChange={(event) => handleChange(name, event.target.value)}
+            // onChange={this.handleChange}
           />
         </Form.Field>
         <Form.Group widths="equal">
@@ -45,14 +51,16 @@ class AddGearForm extends Component {
             name="level"
             value={level}
             options={levelOptions}
-            onChange={this.handleChange}
+            onChange={(event) => handleChange(name, event.target.value)}
+            // onChange={this.handleChange}
           />
           <Form.Field
             control={Select}
             name="available"
             value={available}
             options={availableOptions}
-            onChange={this.handleChange}
+            onChange={(event) => handleChange(name, event.target.value)}
+            // onChange={this.handleChange}
           />
         </Form.Group>
       </Form>
--- src/components/addGearModal.js
+++ src/components/addGearModal.js
@@ -1,10 +1,20 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Container, Modal, Button, Icon } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import AddGearForm from "./addGearForm";
 
 function AddGearModal() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState("");
+  const [level, setLevel] = useState("1");
+  const [available, setAvailable] = useState("1");
+
+  const handleChange = (name, value) => {
+    //this.setState({ [name]: value });
+    console.log("Name: ", name);
+    console.log("Value: ", value);
+  };
+
   return (
     <Container>
       <Modal
@@ -19,7 +29,12 @@ function AddGearModal() {
       >
         <Modal.Header>Add New Gear</Modal.Header>
         <Modal.Content>
-          <AddGearForm />
+          <AddGearForm
+            name={name}
+            level={level}
+            avilable={available}
+            onChange={() => handleChange}
+          />
         </Modal.Content>
         <Modal.Actions>
           <Button color="black" onClick={() => setOpen(false)}>
