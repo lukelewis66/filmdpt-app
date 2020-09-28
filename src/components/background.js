c0f4e2e4e7dd06a2464a480f55692463901592d4
Luke Lewis <lukemarius@gmail.com>
2020-09-28 15:06:37 -0700

    made top splash an accordion of images

--- src/components/background.js
+++ src/components/background.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { Carousel } from "react-bootstrap";
 import "../index.css";
 import bg from "../images/background-image.jpg";
+import bg2 from "../images/background-2.jpg";
+import bg3 from "../images/background-3.jpg";
 
 const Background = ({ activepage }) => {
   const [active, setActive] = useState(activepage);
@@ -22,17 +25,43 @@ const Background = ({ activepage }) => {
     }
   };
   return (
-    <div
-      style={{
-        backgroundImage: `url("${bg}")`,
-        backgroundSize: "cover",
-        height: "50vh",
-        backgroundPosition: "center",
-        position: "relative",
-      }}
-    >
+    <div style={{ height: "50vh" }}>
+      <Carousel>
+        <Carousel.Item>
+          <div
+            style={{
+              backgroundImage: `url("${bg}")`,
+              backgroundSize: "cover",
+              height: "50vh",
+              backgroundPosition: "center",
+              position: "relative",
+            }}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <div
+            style={{
+              backgroundImage: `url("${bg2}")`,
+              backgroundSize: "cover",
+              height: "50vh",
+              backgroundPosition: "center",
+              position: "relative",
+            }}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <div
+            style={{
+              backgroundImage: `url("${bg3}")`,
+              backgroundSize: "cover",
+              height: "50vh",
+              backgroundPosition: "center",
+              position: "relative",
+            }}
+          />
+        </Carousel.Item>
+      </Carousel>
       <h5 className="bg-header">{getHeader()}</h5>
-      {/* <Users users={users} /> */}
     </div>
   );
 };
